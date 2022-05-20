4a11beac98eea376faf6e40f35a0a1b593968274
Anta <felloso666@gmail.com>
2022-05-20 12:37:56 +0300

    added lab_2

---
+++ Lab_2_XML/Bank/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class Client
+    {
+        public string name { get;}
+        public int code { get; }
+        private int IPN { get; }
+        public string email { get; }
+
+        //constructor
+        public Client(string new_name, int new_code, int new_IPN, string new_email)
+        {
+            this.name = new_name;
+            this.code = new_code;
+            this.IPN = new_IPN;
+            this.email = new_email;
+        }
+    }
+}
---
+++ Lab_2_XML/Bank/Deposit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class Deposit
+    {
+        public int code { get; }
+        public int client_code { get; }
+        public int bank_account { get; }
+        public Currency currency { get; }
+        public decimal sum { get; }
+        public float percent { get; }
+        public DateTime start { get; }
+        public DateTime end { get; }
+
+    //constructor
+    public Deposit(int new_code, int new_client, int new_bank,
+        Currency new_currency, decimal new_sum, float new_percent,
+            DateTime new_start, DateTime new_end)
+        {
+            this.code = new_code;
+            this.client_code = new_client;
+            this.bank_account = new_bank;
+            this.currency = new_currency;
+            this.sum = new_sum;
+            this.percent = new_percent;
+            this.start = new_start;
+            this.end = new_end;
+        }
+    }
+}
