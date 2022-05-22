33cabce2033d300798b9c00358e05b0a7e227994
Anta <felloso666@gmail.com>
2022-05-22 20:25:23 +0300

    finished lab2

--- Lab_2_XML/Lab_2_XML/Program.cs
+++ Lab_2_XML/Lab_2_XML/Program.cs
@@ -239,140 +239,100 @@ namespace MyApp
 
 
             Console.WriteLine();
-            Console.WriteLine($" Clients with code 7234 deposits in UAH sum {q9} ");
-                
-            
+            Console.WriteLine($" Clients with code 7234 deposits in UAH sum is {q9} ");
 
 
-            /*  Console.WriteLine("Clients:");
-              var q1 = from client in clients
-                       select client.name;
-              foreach (var client in q1)
-                  Console.WriteLine(client);
+            var q10 = from client in q1
+                      join credit in q2 on
+                      client.code equals credit.clientCode
+                      select new
+                      { name = client.name,
+                        sum = credit.sum,
+                        currency = credit.currency
+                      };
 
-              Console.WriteLine("\nNew in LINQ:");
-              var q2 = from client in clients
-                       select new { name = "Mark", age = 18 };
-              foreach (var client in q2)
-              {
-                  Console.WriteLine(client);
-                  break;
-              }
+            Console.WriteLine();
+            foreach (var i in q10){
+                Console.WriteLine(i);
+            }
 
-              Console.WriteLine("\nClients with code > 5000:");
-              var q3 = from client in clients
-                       where client.code > 5000
-                       select client.name;
-              foreach (var client in q3)
-                  Console.WriteLine(client);
+            var q11 = from client in q10
+                      group client by client.name;
+                    
 
-              Console.WriteLine("\nSorting:");
-              var q4 = from client in clients
-                       where client.code > 1000
-                       orderby client.code
-                       select client.code;
-              foreach (var code in q4)
-                  Console.WriteLine(code);
+            Console.WriteLine();
+            foreach (var group in q11)
+            {
+                Console.WriteLine(group.Key);
+                foreach (var client in group)
+                Console.WriteLine(client);
+            }
 
-              Console.WriteLine("\nCredits sum of credits in UAH:");
-              var q5 = from credit in credits
-                       where credit.currency is Currency.UAH
-                       orderby credit.sum
-                       select credit.sum;
-              foreach (var sum in q5)
-                  Console.WriteLine(sum);
+            var q12 = from mygroup in q11
+                      select new
+                      {
+                          name = mygroup.Key,
+                          sumUSD = mygroup
+                                    .Where(x => x.currency == "USD")
+                                    .Sum(x => Convert.ToInt32(x.sum)),
+                          sumUAH = mygroup
+                                    .Where(x => x.currency == "UAH")
+                                    .Sum(x => Convert.ToInt32(x.sum))
+                      };
 
-              Console.WriteLine("\nSorting with expanding methods:");
-              var q6 = deposits.Where((deposit) =>
-              {
-                  return deposit.sum > 200 && deposit.currency == Currency.USD;
-              }).OrderByDescending(deposit => deposit.sum).ThenByDescending(deposit => deposit.client_code);
-              foreach (var deposit in q6) Console.WriteLine(deposit.sum);
+            Console.WriteLine();
+            foreach (var i in q12)
+            {
+                Console.WriteLine(i);
+            }
 
-              Console.WriteLine("\nSkipWhile and TakeWhile");
-              int[] intArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
-              var q7 = intArray.SkipWhile(x => (x < 3)).TakeWhile(x => x <= 5);
-              foreach (var x in q7)
-                  Console.WriteLine(x);
+            var q13 = q3.Select(deposit => new
+            {
+                clientCode = deposit.Element("clientCode").Value,
+                sum = Convert.ToInt32(deposit.Element("depositSum").Value),
+                currency = deposit.Element("depositCurrency").Value
+            });
 
-              Console.WriteLine("\nINNER JOIN");
-              var q8 = from client in clients
-                       join deposit in deposits on client.code equals deposit.client_code
-                       select new { name = client.name, sum = deposit.sum };
-              foreach (var x in q8)
-                  Console.WriteLine(x.name + " deposit sum: " + x.sum);
+            Console.WriteLine();
+            foreach (var i in q13)
+            {
+                Console.WriteLine(i);
+            }
 
-              Console.WriteLine("\nJOIN with WHERE");
-              var q9 = from client in clients
-                       from deposit in deposits
-                       where client.code == deposit.client_code
-                       select new { name = client.name, sum = deposit.sum };
-              foreach (var x in q9)
-                  Console.WriteLine(x.name + " deposit sum: " + x.sum);
+            var q14 = from client in q1
+                      join deposit in q13 on
+                      client.code equals deposit.clientCode
+                      select new
+                      {
+                          name = client.name,
+                          sum = deposit.sum,
+                          currency = deposit.currency
+                      };
 
-              Console.WriteLine("\nGroup Join");
-              var q10 = from client in clients
-                        join deposit in deposits on client.code equals deposit.client_code into temp
-                        select new { name = client.name, depositGroup = temp };
-              foreach (var client in q10)
-              {
-                  Console.WriteLine(client.name);
-                  foreach (var deposit in client.depositGroup)
-                      Console.WriteLine("   " + deposit.sum);
-              }
+            Console.WriteLine();
+            foreach (var i in q14)
+            {
+                Console.WriteLine(i);
+            }
 
-              Console.WriteLine("\nCross Join and Group Join");
-              var q11 = from client in clients
-                        join deposit in deposits on client.code equals deposit.client_code into temp
-                        from t in temp
-                        select new { name = client.name, sum = t.sum, cnt = temp.Count() };
-              foreach (var x in q11)
-                  Console.WriteLine(x);
+            var q15 = from client in q14
+                      group client by client.name into mygroup
+                      select new
+                      {
+                          name = mygroup.Key,
+                          sumUSD = mygroup
+                                    .Where(x => x.currency == "USD")
+                                    .Sum(x => Convert.ToInt32(x.sum)),
+                          sumUAH = mygroup
+                                    .Where(x => x.currency == "UAH")
+                                    .Sum(x => Convert.ToInt32(x.sum))
+                      };
 
-              Console.WriteLine("\nOuter Join");
-              var q12 = from client in clients
-                        join deposit in deposits on client.code equals deposit.client_code into temp
-                        from t in temp.DefaultIfEmpty()
-                        select new { name = client.name, sum = ((t == null) ? 0 : t.sum) };
-              foreach (var x in q12)
-                  Console.WriteLine(x);
-
-              Console.WriteLine("\nGrouping by currency");
-              var q13 = from credit in credits
-                        group credit by credit.currency into g
-                        select new { Key = g.Key, Values = g };
-              foreach (var credit in q13)
-              {
-                  Console.WriteLine(credit.Key);
-                  foreach (var s in credit.Values)
-                      Console.WriteLine(" " + s.sum);
-              }
-
-              Console.WriteLine("\nGrouping with aggregation functions ");
-              var q14 = from credit in credits
-                        group credit by credit.client_code into g
-                        select new { Key = g.Key, Values = g, cnt = g.Count(), cnt1 = g.Count(credit => credit.sum > 500), sum = g.Sum(credit => credit.sum), min = g.Min(credit => credit.sum) };
-              foreach (var credit in q14)
-              {
-                  Console.WriteLine("Client code: " + credit.Key + " Number: " + credit.cnt + " Sum: " + credit.sum + " Min: " + credit.min);
-                  foreach (var y in credit.Values)
-                      Console.WriteLine(" " + y.sum);
-              }
-
-              Console.WriteLine("\nCredit sums of clients whose name starts with 'M' showed by their e-mails");
-              var q15 = from client in clients
-                        join credit in credits on client.code equals credit.client_code into temp
-                        where client.name.StartsWith("M")
-                        select new { name = client.name, email = client.email, Values = temp };
-              foreach (var client in q15)
-              {
-                  Console.WriteLine("email: " + client.email);
-                  foreach (var y in client.Values)
-                      Console.WriteLine("Sum: " + y.sum + " Currency type: " + y.currency);
-
-              }*/
-
-
+            Console.WriteLine();
+            foreach (var i in q15)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
